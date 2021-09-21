9a30bc88c4f5f1b0de485557887a1ea12a248f19
Karthik Raj K <85018219+Karthik-Ross@users.noreply.github.com>
2021-09-21 10:04:49 +0530

    Calculating the simple interest using python

---
+++ interest.py
@@ -0,0 +1,7 @@
+principal = float(input("Enter the principal amount "))
+rate = float(input("Enter the Rate % "))
+time = float(input("Enter the time taken "))
+
+simple_Interest = (principal * rate * time) / 100
+
+print("the interest is ",simple_Interest)
